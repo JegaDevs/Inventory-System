fee92ae2fa0f33e1bf317cb50d8d98fb66c41f50
Otávio Fidelix <otavio080302@gmail.com>
2025-03-19 12:02:01 -0300

    added 2 more items

--- Inventory System/Assets/Scripts/InventorySystem/InventoryItem.cs
+++ Inventory System/Assets/Scripts/InventorySystem/InventoryItem.cs
@@ -14,7 +14,7 @@ namespace Jega.InventorySystem
 
         public int ID => id;
 
-        public enum ItemType : byte { Weapon, Clothing, Object }
+        public enum ItemType : byte { Weapon, Drink, Object }
         
         public int GetCustomSavedAmount(string customKey, int startingAmount)
         {
